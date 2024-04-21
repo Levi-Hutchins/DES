9f5bc07f98428be699e9f0074793fcaf09b862ed
Levi Hutchins <levihutchins0@gmail.com>
2024-04-21 12:16:16 +1000

    SBoxes 1 and 2, c++20 version

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 CC=g++
-CFLAGS=-c -Wall 
+CFLAGS=-c -Wall -std=c++20
 LDFLAGS=
 SOURCES=test.cpp
 OBJECTS=$(SOURCES:.cpp=.o)
--- test.cpp
+++ test.cpp
@@ -1,7 +1,14 @@
 
 #include <iostream>
-
+#include "SBoxes.h"
 int main() {
-    std::cout << "Hello World!" << std::endl;
+
+    // SBoxes::S1; - How to get static prop
+    for(const auto& row: SBoxes::S1){
+        for(int col: row){
+            cout << col << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
